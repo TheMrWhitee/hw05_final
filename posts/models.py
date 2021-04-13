e4efcd31c1c28a6efbae19f5794fdd36b8409320
TheMrWhitee <ing-telemex@yandex.ru>
2021-04-13 14:06:42 +0300

    add Follow in admin

--- posts/models.py
+++ posts/models.py
@@ -51,8 +51,10 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name='follower')
+                             on_delete=models.SET_NULL,
+                             related_name='follower',
+                             null=True)
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='following')
+                               on_delete=models.SET_NULL,
+                               related_name='following',
+                               null=True)
