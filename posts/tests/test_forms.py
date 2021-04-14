96af94d36c79cde7ce0d0be11568d50d103512cd
Sergei Belov <belov_sv89@mail.ru>
2021-04-15 00:29:11 +0300

    assertFormErrors

--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -29,10 +29,6 @@ class PostFormTests(TestCase):
                                           content=cls.small_gif,
                                           content_type='image/gif')
 
-        # cls.text_file = SimpleUploadedFile(name='not_image.txt',
-        #                                    content='some text',
-        #                                    content_type='text/txt')
-
         cls.author = User.objects.create_user(username='TestUser')
         cls.post = Post.objects.create(author=cls.author,
                                        text='Тестовый пост')
@@ -64,10 +60,10 @@ class PostFormTests(TestCase):
     def test_create_post_with_not_image_file_in_form(self):
         """Нельзя загрузить другой файл вместо картинки."""
         form_data = {'text': 'Тестовый пост из формы',
-                     'image': 'some text'}
+                     'image': 'text file'}
         response = self.authorized_client.post(reverse('posts:new_post'),
                                                data=form_data)
-        self.assertFormError(response, 'form', 'image', 'текст ошибки')
+        self.assertFormError(response, 'form', 'image', 'image only')
 
     def test_edit_post_in_form(self):
         """проверка редактирования поста."""
