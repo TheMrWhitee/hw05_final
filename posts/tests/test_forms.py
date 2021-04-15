594557eb92c407da19689cbce3b157598a0de838
TheMrWhitee <ing-telemex@yandex.ru>
2021-04-15 19:34:14 +0300

    for review 1

--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -29,6 +29,10 @@ class PostFormTests(TestCase):
                                           content=cls.small_gif,
                                           content_type='image/gif')
 
+        cls.text_file = SimpleUploadedFile(name='text.txt',
+                                           content='',
+                                           content_type='text/plain')
+
         cls.author = User.objects.create_user(username='TestUser')
         cls.post = Post.objects.create(author=cls.author,
                                        text='Тестовый пост')
@@ -60,7 +64,7 @@ class PostFormTests(TestCase):
     def test_create_post_with_not_image_file_in_form(self):
         """Нельзя загрузить другой файл вместо картинки."""
         form_data = {'text': 'Тестовый пост из формы',
-                     'image': 'text file'}
+                     'image': self.text_file}
         response = self.authorized_client.post(reverse('posts:new_post'),
                                                data=form_data,
                                                follow=True)
