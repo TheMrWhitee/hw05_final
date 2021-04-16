f0fbdf62ef6d2f1359f98b525ff2224e6fa50a95
TheMrWhitee <ing-telemex@yandex.ru>
2021-04-16 14:08:31 +0300

    del empty lines

--- posts/tests/test_forms.py
+++ posts/tests/test_forms.py
@@ -19,22 +19,18 @@ class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-
         cls.small_gif = (b'\x47\x49\x46\x38\x39\x61\x02\x00'
                          b'\x01\x00\x80\x00\x00\x00\x00\x00'
                          b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
                          b'\x00\x00\x00\x2C\x00\x00\x00\x00'
                          b'\x02\x00\x01\x00\x00\x02\x02\x0C'
                          b'\x0A\x00\x3B')
-
         cls.uploaded = SimpleUploadedFile(name='small.gif',
                                           content=cls.small_gif,
                                           content_type='image/gif')
-
         cls.text_file = SimpleUploadedFile(name='text.txt',
                                            content='',
                                            content_type='text/plain')
-
         cls.author = User.objects.create_user(username='TestUser')
         cls.post = Post.objects.create(author=cls.author,
                                        text='Тестовый пост')
